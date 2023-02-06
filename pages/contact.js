6069815428fa3d2553921793f672a94d0a5345b9
CodeSandbox <support@codesandbox.io>
2023-02-06 20:25:22 +0000

    Initial commit

---
+++ pages/contact.js
@@ -0,0 +1,51 @@
+import { motion as m } from "framer-motion";
+import { container, item } from "../animation";
+
+export default function Contact() {
+  return (
+    <m.main
+      initial={{ y: "100%" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 0.75, ease: "easeOut" }}
+      exit={{ opacity: 1 }}
+      className="text-gray-900 absolute top-0 left-0 w-full bg-red-400 lg:px-48 px-16"
+    >
+      <div className="my-96 p-1 overflow-hidden">
+        <m.h1
+          animate={{ y: 0 }}
+          initial={{ y: "100%" }}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          className="text-6xl text-center lg:text-right lg:text-9xl"
+        >
+          Let&apos;s Talk
+        </m.h1>
+      </div>
+      <div className="flex gap-40">
+        <div className="lg:text-2xl text-base">
+          <h4>Find me:</h4>
+        </div>
+        <div className="lg:text-6xl text-2xl underline">
+          <m.ul variants={container} intitial="hidden" animate="show">
+            <div className="overflow-hidden">
+              <m.li variants={item} className="pb-2">
+                Twitter
+              </m.li>
+            </div>
+            <div className="overflow-hidden">
+              <m.li variants={item} className="pb-2">
+                Instagram
+              </m.li>
+            </div>
+            <div className="overflow-hidden">
+              <m.li variants={item} className="pb-2">
+                Linkedin
+              </m.li>
+            </div>
+          </m.ul>
+        </div>
+      </div>
+    </m.main>
+  );
+}
+
+// style w-full h-full for class in main removed
